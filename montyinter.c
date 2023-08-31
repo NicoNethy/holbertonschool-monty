e8bd69d0559967f7d8cca98b336d9eb337c890f7
NicoNethy <5761@holbertonstudents.com>
2023-08-30 21:09:03 -0700

    debugging

--- montyinter.c
+++ montyinter.c
@@ -106,22 +106,24 @@ void free_all(stack_t *stack, char *buffer)
  * @argv: index of strings containing the arguments passed on the command line
  * Return: 0 if the program runs correctly
 */
-int main(int argc, char **argv) {
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE); }
+int main(int argc, char **argv) 
+{
 	stack_t *stack = NULL;
-	FILE *file = fopen(argv[1], "r");
-
-	if (file == NULL) {
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE); }
 	char *buffer = NULL, *code = NULL;
 	size_t buffsize = 0;
 	unsigned int line_number = 0;
 
-	while (getline(&buffer, &buffsize, file) != -1) {
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE); }
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL) 
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE); }
+	while (getline(&buffer, &buffsize, file) != -1) 
+	{
 		line_number++;
 		code = strtok(buffer, " \t\n");
 		if (code == NULL)
@@ -147,4 +149,3 @@ int main(int argc, char **argv) {
 	free_all(stack, buffer);
 	return (0);
 	}
-	
