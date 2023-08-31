78ec1c2c0b936618d76ec0f7d58dd74af7a104d2
NicoNethy <5761@holbertonstudents.com>
2023-08-30 20:25:04 -0700

    the file for testing

---
+++ file.m
@@ -0,0 +1,14 @@
+push 0
+push 1
+push 2
+  push 3
+                 pall    
+				 push 4
+	    pop
+				     push 5   
+	    pil
+			    			       push    6  
+			push 7
+			    add
+							pall
+    it is monty time
